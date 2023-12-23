04ebfe51580315053e942e42fd61fb37d79db8f8
singhakshay29 <akshay2898.as@gmail.com>
2023-12-23 20:22:30 +0530

    cart update

--- src/features/cart/cart.js
+++ src/features/cart/cart.js
@@ -39,96 +39,98 @@ export default function Cart() {
 
   return (
     <>
-      <div className="mx-auto max-w-7xl mt-20 px-4 sm:px-6 lg:px-8">
-        <div className=" mx-auto border-t border-gray-200 px-4 py-6 sm:px-6">
-          <h1 className="text-2xl font-bold tracking-tight text-gray-900">
-            All products
-          </h1>
-          <div className="flow-root mt-8">
-            <ul role="list" className="-my-6 divide-y divide-gray-200">
-              {products.map((product) => (
-                <li key={product.id} className="flex py-6">
-                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-
-                  <div className="ml-4 flex flex-1 flex-col">
-                    <div>
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <h3>
-                          <a href={product.href}>{product.name}</a>
-                        </h3>
-                        <p className="ml-4">{product.price}</p>
-                      </div>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {product.color}
-                      </p>
-                    </div>
-                    <div className="flex flex-1 items-end justify-between text-sm">
-                      <div className="text-gray-500">
-                        <label
-                          htmlFor="quantity"
-                          className="inline mr-5 text-sm font-medium leading-6 text-gray-900">
-                          Qty
-                        </label>
-
-                        <select id="quantity">
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                        </select>
+      <>
+        <div>
+          <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+              <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
+                Cart
+              </h1>
+              <div className="flow-root">
+                <ul role="list" className="-my-6 divide-y divide-gray-200">
+                  {products.map((product) => (
+                    <li key={product.id} className="flex py-6">
+                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <img
+                          src={product.imageSrc}
+                          alt={product.imageAlt}
+                          className="h-full w-full object-cover object-center"
+                        />
                       </div>
 
-                      <div className="flex">
-                        <button
-                          type="button"
-                          className="font-medium text-indigo-600 hover:text-indigo-500">
-                          Remove
-                        </button>
+                      <div className="ml-4 flex flex-1 flex-col">
+                        <div>
+                          <div className="flex justify-between text-base font-medium text-gray-900">
+                            <h3>
+                              <a href={product.href}>{product.name}</a>
+                            </h3>
+                            <p className="ml-4">{product.price}</p>
+                          </div>
+                          <p className="mt-1 text-sm text-gray-500">
+                            {product.color}
+                          </p>
+                        </div>
+                        <div className="flex flex-1 items-end justify-between text-sm">
+                          <div className="text-gray-500">
+                            <label
+                              htmlFor="quantity"
+                              className="inline mr-5 text-sm font-medium leading-6 text-gray-900">
+                              Qty
+                            </label>
+                            <select>
+                              <option value="1">1</option>
+                              <option value="2">2</option>
+                            </select>
+                          </div>
+
+                          <div className="flex">
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500">
+                              Remove
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+              <div className="flex justify-between text-base font-medium text-gray-900">
+                <p>Subtotal</p>
+                <p>$262.00</p>
+              </div>
+              <p className="mt-0.5 text-sm text-gray-500">
+                Shipping and taxes calculated at checkout.
+              </p>
+              <div className="mt-6">
+                <a
+                  href="/"
+                  className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                  Checkout
+                </a>
+              </div>
+              <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                <p>
+                  or
+                  <Link to="/">
+                    <button
+                      type="button"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                      onClick={() => setOpen(false)}>
+                      Continue Shopping
+                      <span aria-hidden="true"> &rarr;</span>
+                    </button>
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className=" mx-auto border-t border-gray-200 px-4 py-6 sm:px-6">
-          <div className="flex justify-between text-base font-medium text-gray-900">
-            <p>Subtotal</p>
-            <p>$262.00</p>
-          </div>
-          <p className="mt-0.5 text-sm text-gray-500">
-            Shipping and taxes calculated at checkout.
-          </p>
-          <div className="mt-6">
-            <a
-              href="/"
-              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-              Checkout
-            </a>
-          </div>
-          <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-            <p>
-              or
-              <Link to="/">
-                <button
-                  type="button"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                  onClick={() => setOpen(false)}>
-                  Continue Shopping
-                  <span aria-hidden="true"> &rarr;</span>
-                </button>
-              </Link>
-            </p>
-          </div>
-        </div>
-      </div>
+      </>
     </>
   );
 }
